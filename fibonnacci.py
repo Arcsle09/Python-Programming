ae29adcc42fa5c34c03a8cd1528045fa472f089e
Rakesh Chaudhary <arcsle09@gmail.com>
2018-06-01 18:15:29 +0530

    Add files via upload

---
+++ fibonnacci.py
@@ -0,0 +1,11 @@
+def fibonnacci(n):
+    if n <= 1:
+        return(1)
+    else:
+        return(fibonnacci(n-1) + fibonnacci(n-2))
+
+n = int(input('Enter the number to produce finbonnacci series: '))
+
+for i in range(0,n):
+    print(fibonnacci(i), end = " ")
+    
---
+++ towerofhanoi.py
@@ -0,0 +1,23 @@
+def towerofhanoi(n,start,aux,end):
+    if n >= 1:
+        towerofhanoi(n-1,start,end,aux)
+        print("move " + start + " to " + end)
+        towerofhanoi(n-1,aux,start,end)
+
+######################output#################
+#towerofhanoi(4,'start','aux','end')
+#move start to aux
+#move start to end
+#move aux to end
+#move start to aux
+#move end to start
+#move end to aux
+#move start to aux
+#move start to end
+#move aux to end
+#move aux to start
+#move end to start
+#move aux to end
+#move start to aux
+#move start to end
+#move aux to end
