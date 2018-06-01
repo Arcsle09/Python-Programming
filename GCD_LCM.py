dfcf7dca99126825e3605bf395b9a5fbf2fdb6bc
Rakesh Chaudhary <arcsle09@gmail.com>
2018-06-01 22:43:03 +0530

    Add files via upload

---
+++ GCD_LCM.py
@@ -0,0 +1,14 @@
+def GCD(a,b):
+    if a == b:
+        return(a)
+    elif a % b == 0:
+        return(b)
+    elif b % a == 0:
+        return(a)
+    elif a > b:
+        return(GCD(a % b,b))
+    elif a < b:
+        return(GCD(a,b % a))
+    
+def LCM(a,b):
+    return(a*b/GCD(a,b))
