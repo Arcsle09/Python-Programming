116ebcc6ae133604c7131241be1e47e1cc80e277
Rakesh Chaudhary <arcsle09@gmail.com>
2018-06-04 22:46:07 +0530

    Add files via upload

---
+++ rock_paper_scissors.py
@@ -0,0 +1,30 @@
+# Rock-Paper-Scissors Game 
+
+from random import *
+
+userchoice = input("Do you choose rock,paper or scissors? ")
+
+items = ['rock','paper','scissors']
+
+computerchoice = sample(items,1)
+
+def compare(computerchoice,userchoice):
+    if computerchoice[0] == userchoice:
+        print('It is tie !')
+    elif computerchoice[0] == 'rock':
+        if userchoice == 'scissors':
+            print('rock wins !')
+        else:
+            print('paper wins !')
+    elif computerchoice[0] == 'paper':
+        if userchoice == 'rock':
+            print('paper wins !')
+        else:
+            print('scissors win !')
+    elif computerchoice[0] == 'scissors':
+        if userchoice == 'rock':
+            print('rock wins !')
+        else:
+            print('scissors win !')
+
+compare(computerchoice,userchoice)
