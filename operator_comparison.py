ee4b408621ec74feed5974c1d0f49c3fca336d80
Rakesh Chaudhary <arcsle09@gmail.com>
2018-06-23 20:05:07 +0530

    Add files via upload

---
+++ operator_comparison.py
@@ -0,0 +1,14 @@
+# program to prove "==" is not same as "is" 
+# In python, the variable acts like an object.
+# It can be compared with pointer in C programming. 
+# the variable(or object) stores the memory address of value being stored inside of it.
+
+x = 42
+
+y = 42.0 
+
+#x == y
+#Out[327]: True
+
+#x is y
+#Out[328]: False
