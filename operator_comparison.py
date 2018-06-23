d0d772ca2c7fd191b71133f9ee1a622e3b115ea6
Rakesh Chaudhary <arcsle09@gmail.com>
2018-06-23 20:09:51 +0530

    Update operator_comparison.py

--- operator_comparison.py
+++ operator_comparison.py
@@ -1,6 +1,9 @@
 # program to prove "==" is not same as "is" 
+
 # In python, the variable acts like an object.
+
 # It can be compared with pointer in C programming. 
+
 # the variable(or object) stores the memory address of value being stored inside of memory. 
 
 x = 42
@@ -12,3 +15,12 @@ y = 42.0
 
 #x is y
 #Out[328]: False
+
+
+#the variable makes a decision based on the data type of the values. 
+
+# If the same values have same data type,
+        # then both x and y variables will reference to the same memory address.
+#  else
+        # x and y will point different memory addresses and hence different values.
+
