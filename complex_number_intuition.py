7f9d99303b2da035e8e4380669cb3d561a604e14
Rakesh Chaudhary <arcsle09@gmail.com>
2018-06-26 18:15:22 +0530

    Add files via upload

---
+++ complex_number_intuition.py
@@ -0,0 +1,15 @@
+x = input('Enter the complex number:')
+
+#x input will be stored as string and we need to remove
+# spaces from the complex number before using complex function.
+
+#if the user does not enter spaces,it will do nothing. 
+
+x = complex(x.replace(" ", ""))
+
+x = (x.real,x.imag)
+
+if x[0] > x[1]:
+    print('%f is the greater.' %x[0])
+else:
+    print('%f is the greater.' %x[1])
