b0d71fb9dca92d5360c793b48404c7d4908f864b
Rakesh Chaudhary <arcsle09@gmail.com>
2018-09-19 14:26:14 +0530

    Update Collatz_Sequence.py

--- Collatz_Sequence.py
+++ Collatz_Sequence.py
@@ -7,8 +7,11 @@ while n != 1:
   print(n,end=", ")
   if n % 2 == 0:
     n = n // 2
+# In Python 3.0, 5 / 2 will return 2.5 and 5 // 2 will return 2. 
+# The former is floating point division, and the latter is floor division, sometimes also called integer division.
   else:
     n = n*3 + 1
 print(n,end = ".\n")
+
 
 # More Mathematical Insights about the algorithm: #http://mathworld.wolfram.com/CollatzProblem.html
