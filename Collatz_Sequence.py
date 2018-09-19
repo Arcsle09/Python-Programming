d2d5267c6bbdfa774a00db1b811b45dc4362d1c3
Rakesh Chaudhary <arcsle09@gmail.com>
2018-09-19 13:22:52 +0530

    Collatz_Sequence
    Collatz Sequence from "The Collatz Problem"

---
+++ Collatz_Sequence.py
@@ -0,0 +1,14 @@
+#Collatz Sequence from "The Collatz Problem"
+
+n = int(input("""Enter any positive number to create "The Collatz Sequence":"""))
+print(end="\n")
+
+while n != 1:
+  print(n,end=", ")
+  if n % 2 == 0:
+    n = n // 2
+  else:
+    n = n*3 + 1
+print(n,end = ".\n")
+
+#More Mathematical Insights about the algorithm: #http://mathworld.wolfram.com/CollatzProblem.html
