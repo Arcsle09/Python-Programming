283ad03897d0ff36bbade0265be498213321fa7a
Rakesh Chaudhary <arcsle09@gmail.com>
2018-09-19 19:07:06 +0530

    Square Root Program - Newton Raphson Method

---
+++ Square_Root_Newton_Raphson.py
@@ -0,0 +1,14 @@
+#Newton Raption Method to find out the square root 
+
+n = int(input("Enter the number:"))
+
+threshold = 0.0001 
+
+approximation = n/2
+
+while True:
+  better = (approximation + n/approximation)/2 
+  if abs(approximation - better) < threshold:
+    print(better)
+    break
+  approximation = better
