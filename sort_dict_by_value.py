b9f3459695e9c203fd292e72cb62e9295f0fa224
Rakesh Chaudhary <arcsle09@gmail.com>
2018-09-29 12:30:32 +0530

    Update sort_dict_by_value.py

--- sort_dict_by_value.py
+++ sort_dict_by_value.py
@@ -5,8 +5,8 @@ first_dict = dict()
 n = int(input("How many items you need in a list: "))
 
 for i in range(0,n):
-  name = input("Enter the key: ")
-  age = int(input("Enter the value: "))
+  name = input("Enter the name: ")
+  age = int(input("Enter the age: "))
   first_dict[name] = age
 
 print("The list of Youngest to Oldest People:")
