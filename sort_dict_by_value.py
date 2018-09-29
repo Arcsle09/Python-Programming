aae8413bd8eeac5fbef4a523e07d186673a5270b
Rakesh Chaudhary <arcsle09@gmail.com>
2018-09-29 12:27:31 +0530

    Sorts the dictionary by value

---
+++ sort_dict_by_value.py
@@ -0,0 +1,15 @@
+
+# Write a script to sort a dictionary according to Value
+
+first_dict = dict()
+n = int(input("How many items you need in a list: "))
+
+for i in range(0,n):
+  name = input("Enter the key: ")
+  age = int(input("Enter the value: "))
+  first_dict[name] = age
+
+print("The list of Youngest to Oldest People:")
+for i in sorted(first_dict.values()):
+  print(list(first_dict.keys())[list(first_dict.values()).index(i)],":",i)
+
