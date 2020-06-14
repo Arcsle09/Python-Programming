eda1b651dc3e0cc75b878a5d48038fb82ccfaf27
Rakesh Chaudhary <arcsle09plus@gmail.com>
2020-06-14 07:22:31 +0530

    Add files via upload

---
+++ ALGORITHMS/pair_sum.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 15:42:44 2020
+
+@author: chra8017
+"""
+
+#Arrpay pair sum 
+#Given an integer array,output all the unique pairs that sum up to a specific value k
+#so the input:
+#pair_sum([1,3,2,2],4)
+#would return 2 pairs:
+#    (1,3) and (2,2)
+
+def pair_sum(array,k):
+    if len(array) < 2:
+        return print("Too Small")
+    seen = set()
+    output = set()
+    
+    for num in array:
+        target = k - num
+        
+        if target not in seen:
+            seen.add(num)
+        else:
+            output.add((min(num,target),max(num,target)))
+    print('\n'.join(map(str,list(output))))
+pair_sum([1,3,2,2],4)
