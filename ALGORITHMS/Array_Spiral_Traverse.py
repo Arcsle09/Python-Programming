a622f8c51f67050faa34c0602f4b9cb0f688e47c
Rakesh Chaudhary <arcsle09@gmail.com>
2020-08-02 22:16:08 +0530

    Update Array_Spiral_Traverse.py

--- ALGORITHMS/Array_Spiral_Traverse.py
+++ ALGORITHMS/Array_Spiral_Traverse.py
@@ -8,29 +8,26 @@ Created on Sun Aug  2 17:35:33 2020
 #Description: Write the array using spiral path.
 
 def SpiralTraverse(array):
-    import numpy as np
-    array = np.empty([4,4])
+    result = []
     startRow ,endRow = 0,len(array) - 1
     startCol, endCol = 0,len(array[0]) - 1
     
     while startRow <= endRow and startCol <= endCol:
         for col in range(startCol,endCol+1):
-            print(array[startRow][col])
-            print(" ")
-        
+            result.append(array[startRow][col])
+            
         for row in range(startRow + 1,endRow +1):
-            print(array[row][endCol])
-            print(" ")
-    
+            result.append(array[row][endCol])
+
         for col in reversed(range(startCol,endCol)):
-            print(array[endRow][col])
-            print(" ")
+            result.append(array[endRow][col])
         
         for row in reversed(range(startRow+1,endRow)):
-            print(array[row][startCol])
-            print(" ")
+            result.append(array[row][startCol])
         
         startRow = startRow + 1
         endRow  = endRow - 1
         startCol = startCol + 1
         endCol = endCol - 1
+    
+    return result
